0690818151f36bb278fb6e1fddf38d04f9741108
ByungWoo Koh <kohbwoo@gmail.com>
2021-07-29 22:14:24 +0900

    파이썬으로 uart 읽어서 변수 입력 완성

--- Atmega328P-PU/Serial-Setting/Serial-Setting.ino
+++ Atmega328P-PU/Serial-Setting/Serial-Setting.ino
@@ -2,15 +2,19 @@ void setup() {
   Serial.begin(9600);
 }
 
+//W:시간 T:온도 H:습도 D:먼지 C:Co2
 void loop() {
-  
-  Serial.println("Time:1201,Temp:11.5,Humi:41.5,Dust:41");
+  Serial.println("W:1201,T:11.5,H:41.5,D:51,C:501,");
   delay(1000);
-  Serial.println("Time:1202,Temp:12.5,Humi:42.5,Dust:42");
+  Serial.println("W:1202,T:12.5,H:42.5,D:52,C:502,");
   delay(1000);
-  Serial.println("Time:1203,Temp:13.5,Humi:43.5,Dust:43");
+  Serial.println("W:1203,T:13.5,H:43.5,D:53,C:503,");
   delay(1000);
-  Serial.println("Time:1204,Temp:14.5,Humi:44.5,Dust:44");
+  Serial.println("W:1204,T:14.5,H:44.5,D:54,C:504,");
+  delay(1000);
+  Serial.println("W:1205,T:15.5,H:45.5,D:55,C:505,");
+  delay(1000);
+  Serial.println("W:1206,T:16.5,H:46.5,D:56,C:506,");
   delay(1000);
 
 }
