d1d045d547623da86d71fd0ffedd8bcadab561d2
ByungWoo Koh <kohbwoo@gmail.com>
2021-07-31 23:05:53 +0900

    변수 값 랜덤 할당

--- Atmega328P-PU/Serial-Setting/Serial-Setting.ino
+++ Atmega328P-PU/Serial-Setting/Serial-Setting.ino
@@ -1,4 +1,9 @@
+
 int time = 0;
+int temp = 0;
+int humi = 0;
+int dust = 0;
+int co2 = 0;
 void setup() {
   Serial.begin(9600);
   
@@ -14,6 +19,8 @@ void loop() {
     time = 0;
   }
 
+
+
   
   Serial.print("W:");
   if (time < 10){
@@ -26,9 +33,35 @@ void loop() {
     Serial.print("0");
   }
 
+
+
+
   Serial.print(time);
-  Serial.println(",T:11.5,H:41.5,D:51,C:501,");
+  Serial.print(",T:");
+  temp = random(0,99);
+  Serial.print(temp);
+
+  Serial.print(".5,H:");
+  humi = random(0,99);
+  Serial.print(humi);
+
+
+
+  Serial.print(".5,D:");
+  dust = random(0,150);
+  Serial.print(dust);
+  
+ 
+  Serial.print(",C:");
+  co2 = random(400,6000);
+  Serial.print(co2);
+
+
+
+  Serial.println(",");
   delay(10000);
+
+  
   time = time+1;
 
 }
