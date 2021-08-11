8060f27daa4d66604c68fcf4fc253acd6cc44a6b
ByungWoo Koh <kohbwoo@gmail.com>
2021-08-11 22:03:47 +0900

    측정주기 변경, Uart 비트레이트 조정

--- Atmega328P-PU/Dust_Sensor/Dust_Sensor.ino
+++ Atmega328P-PU/Dust_Sensor/Dust_Sensor.ino
@@ -3,7 +3,7 @@ float ugm3 = 0;
 int GP2Y1023 = 8;                                            
 void setup() {
   pinMode(GP2Y1023, INPUT);                       
-  Serial.begin(115200);                          
+  Serial.begin(9600);                          
 }
 void loop() {
   pulse = pulseIn(GP2Y1023, LOW, 20000); 
@@ -12,7 +12,7 @@ void loop() {
     Serial.print(ugm3, 4);                       
     Serial.println(" ug/m3");
   }
-  delay(100);
+  delay(1000);
 }
 float pulse2ugm3(unsigned long pulse) {
   float value = (pulse - 1400) / 14.0;              
