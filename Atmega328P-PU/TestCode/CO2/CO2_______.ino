82a2c11362688f782ab04abf52cc0c1073a3edf9
ByungWoo Koh <kohbwoo@gmail.com>
2021-09-10 16:40:36 +0900

    케소드 세그먼트로 출력 코드로 변경

--- Atmega328P-PU/TestCode/CO2/CO2_______.ino
+++ Atmega328P-PU/TestCode/CO2/CO2_______.ino
@@ -19,33 +19,28 @@ int pinG = 8;
 int D1 = 9;
 int D2 = 10;
 int D3 = 11;
-int D4 = 12;int pinA = 2;
-int pinB = 3;
-int pinC = 4;
-int pinD = 5;
-int pinE = 6;
-int pinF = 7;
-int pinG = 8;
-int D1 = 9;
-int D2 = 10;
-int D3 = 11;
 int D4 = 12;
  */
 #include <MsTimer2.h>
 
-int input;
+
+
+
+int input=1234;
 int loopcount = 0;
-float h;
-char addr[4] = {0,0,0,0};
+float co2;
+char addr[4] = {1,2,3,4};
 
 char List_Of_Segments[4] = { 0x01, 0x02, 0x04, 0x08 };
 char List_Of_Segment_Info[10] = { 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xD8, 0x80, 0x98 };
 // the setup routine runs once when you press reset:
 void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Starting..."));
 
   // initialize the digital pins as outputs.
 
-  for (int i = 2; i < 12; i++){
+  for (int i = 3; i < 14; i++){
     pinMode(i, OUTPUT); 
   } 
   pinMode(A0, OUTPUT);
@@ -55,71 +50,71 @@ void setup() {
 
 void Segment(char SegmentNumx16, char PrintNumx16){
   if(SegmentNumx16 & 0x08){
-    digitalWrite(A0,1);
+    digitalWrite(12 ,0);
   } else {
-    digitalWrite(A0,0);
+    digitalWrite(12, 1);
   }
 
   if(SegmentNumx16 & 0x04){
-    digitalWrite(11,1);
+    digitalWrite(11, 0);
   } else {
-    digitalWrite(11,0);
+    digitalWrite(11, 1);
   }
 
   if(SegmentNumx16 & 0x02){
-    digitalWrite(10,1);
+    digitalWrite(10, 0);
   } else {
-    digitalWrite(10,0);
+    digitalWrite(10, 1);
   }
 
   if(SegmentNumx16 & 0x01){
-    digitalWrite(9,1);
+    digitalWrite(9, 0);
   } else {
-    digitalWrite(9,0);
+    digitalWrite(9, 1);
   }
 
 /////////////////////////////////
 
   if(PrintNumx16 & 0x40){
-    digitalWrite(8, 1);
+    digitalWrite(8, 0);
   } else {
-    digitalWrite(8,0);
+    digitalWrite(8, 1);
   }
 
   if(PrintNumx16 & 0x20){
-    digitalWrite(7, 1);
+    digitalWrite(7, 0);
   } else {
-    digitalWrite(7,0);
+    digitalWrite(7, 1);
   }  
 
   if(PrintNumx16 & 0x10){
-    digitalWrite(6, 1);
+    digitalWrite(6, 0);
   } else {
-    digitalWrite(6,0);
+    digitalWrite(6, 1);
   }
   
   if(PrintNumx16 & 0x08){
-    digitalWrite(5, 1);
+    digitalWrite(5, 0);
   } else {
-    digitalWrite(5,0);
+    digitalWrite(5, 1);
   }
 
   if(PrintNumx16 & 0x04){
-    digitalWrite(4, 1);
+    digitalWrite(4, 0);
   } else {
-    digitalWrite(4,0);
+    digitalWrite(4, 1);
   }
 
   if(PrintNumx16 & 0x02){
-    digitalWrite(3, 1);
+    digitalWrite(3, 0);
   } else {
-    digitalWrite(3,0);
+    digitalWrite(3, 1);
   }
 
   if(PrintNumx16 & 0x01){
-    digitalWrite(2, 1);
+    digitalWrite(A0, 0);
   } else {
-    digitalWrite(2, 0);
+    digitalWrite(A0, 1);
   }
   
 }
@@ -137,14 +132,12 @@ void Segmentpush(){
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
 
-  input = 1234;
   addr[0] = input / 1000;
   addr[1] = input % 1000 / 100;
   addr[2] = input % 100 / 10;
   addr[3] = input % 10;
-  delay(1000);
+
 
 
 }
